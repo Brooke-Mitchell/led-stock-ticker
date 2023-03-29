dc80998e90e65b2f638c9d4f689d9077842c17be
Brooke-Mitchell <72291878+Brooke-Mitchell@users.noreply.github.com>
2023-03-28 22:21:09 -0600

    Update loading.py

--- renderer/loading.py
+++ renderer/loading.py
@@ -21,6 +21,12 @@ class Loading(Renderer):
         self.matrix.SetImage(self.canvas)
 
     def render_version(self):
+        x, y = align_text(self.font.getsize('FINBERRY'),
+                          self.matrix.width,
+                          self.matrix.height,
+                          Position.RIGHT,
+                          Position.TOP)
+        self.draw.text((x, y), 'FINBERRY', Color.PURPLE, self.font)
         x, y = align_text(self.font.getsize('Brooke_Mitchell_is_super_cool'),
                           self.matrix.width,
                           self.matrix.height,
@@ -30,15 +36,9 @@ class Loading(Renderer):
         userName = userName.json()
         user = userName[0]['username'] 
         if off_screen(self.matrix.width, self.font.getsize('Brooke_Mitchell_is_super_cool')[0]):
-            self.scroll_text('Brooke_Mitchell_is_super_cool', self.font, Color.GREEN, Color.BLACK, (1, y))
+            self.scroll_text('Brooke_Mitchell_is_super_cool', self.font, Color.GREEN, Color.BLACK, (1, x))
         else:
             self.draw.text((x, y), 'Brooke_Mitchell_is_super_cool', Color.GREEN, self.font)
-        x, y = align_text(self.font.getsize('FINBERRY'),
-                          self.matrix.width,
-                          self.matrix.height,
-                          Position.RIGHT,
-                          Position.TOP)
-        self.draw.text((x, y), 'FINBERRY', Color.PURPLE, self.font)
 
     def render_logo(self):
         img = load_image(LOADING_IMAGE, tuple(self.coords['image']['size']))
